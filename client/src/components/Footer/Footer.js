34e746285b9f905d3478c0d80f3c1a582c4d4a04
nikolaChakarov <chakarovn1975@gmail.com>
2021-06-01 11:28:23 +0300

    video home page

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <p>Nikola Chakarov - Intern in <span>ISOBAR</span> 2021</p>
+            <p>Nikola Chakarov - Intern at <span>ISOBAR</span> 2021</p>
         </footer>
     )
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import ReactPlayer from 'react-player';
+
 import './Home.css';
 
 const Home = () => {
     return (
         <section className="home-section">
-            <h1>Welcome To Home Page</h1>
+            <h1>DENTSU</h1>
+
+
+            <ReactPlayer
+                className="video-dentsu"
+                url="https://youtu.be/T4udpibGlFA"
+                loop={true}
+                controls={true}
+            />
+
+
+
         </section>
     )
 }
 
 export default Home;
+
+
+// https://youtu.be/T4udpibGlFA
